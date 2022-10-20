c38fbc39bd3b83ac55f65c105a49cbf537900a2b
StevensMidnightBlessings <115166209+StevensMidnightBlessings@users.noreply.github.com>
2022-10-20 19:18:10 -0400

    Create quiz1.py

---
+++ quiz1.py
@@ -0,0 +1,36 @@
+'''
+Stay blessed fellas 💯🔫
+
+
+1) What does security analytics refers to?
+
+Ensuring that an enterprise gains visibility over its IT infrastrure, by collecting log and alert events from various endpoint machines and analyzing such events towards detection of an impending cyber-attack. 
+
+
+2) What collaborative learning refers to, in the context of Machine Learning?
+
+Building global machine-learning models over datasets owned and contributed by distinct entities.
+
+
+3) Which of the following statements are correct?
+
+All of the above 
+
+
+4) Under a certain measure of "closeness" and by considering each data point as a singleton cluster, hierarchical clustering refers to iteratively forming new clusters by merging the pair of currently closest clusters, until some termination criteria are met.
+
+True
+
+
+5) The CURE approximate hierarchical clustering approach involves hierarchically clustering over a random sample of the available data, eliminating outliers, and associating any remaining (not-clustered) data points with the closest formatted clusters.
+
+True
+
+
+6) Extra Credit Question
+
+What is the role/importance of the Point-Agnostic Dendrogram? That is, how do the random permutation and the refined cluster output help, in the protocol of Private Two-Party Cluster Analysis?
+
+The importance of the Point-Agnostic Dendrogram is to improve the accuracy against the achieved privacy. Point-Agnostic Dendrogram does this by merging the history of formed joint clusters and useful statistics thereof. By using random permutation and refined cluster outputs, we can increase the security of our system even greater because the number one problem with Private Two-Party Cluster Analysis is security. 
+
+'''
