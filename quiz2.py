9879d4a701cf438ff47c0feb333b850fd17c774a
StevensMidnightBlessings <115166209+StevensMidnightBlessings@users.noreply.github.com>
2022-10-20 19:29:01 -0400

    Create quiz2.py

---
+++ quiz2.py
@@ -0,0 +1,41 @@
+'''
+
+Ya boy got a 2/8... But as Professor Triandopoulos said, "one has to try to fail the course." Fist up for ya boy when you get that 100 ✊💚
+
+
+
+
+1) What does the term Outsourced Computing refer to?
+The computing paradigm where storage, management and processing of one entity's data is delegated to another entity's computing infrastructure.
+
+
+2) Why security becomes crucial in today's computing reality?
+Because due to outsourced computing information "owned" or "consumed" by end users is typically managed outside their trust perimeter by a remote and unknown machine.
+
+
+3) How do the properties of integrity and privacy become relevant to the scenario where your file system is stored by a cloud storage provider, like Dropbox?
+Integrity and privacy mean that at all times your file system is in its correct and secret state as if it was locally stored at your protected hard drive and never was uploaded to the cloud. 
+
+
+4) What does public-key encryption refers to?
+A cryptographic technique for protecting the confidentiality of a message, by using a public recipient-specific key to transmit it as a ciphertext that is intelligible only by the recipient possessing a corresponding secret key
+
+
+5) What does a digital signature refer to?
+A cryptographic technique for checking the intactness of a message, by using a sender-specific secret key to transmit it augmented with a signature that can be successfully verified by any recipient possessing the sender's corresponding public key.
+
+
+6) After a meeting with your manager, a member of a startup NextGenSec and a member of a startup BestEverSec, on the possibility of an acquisition of one of the two companies, you are asked by your manager to securely email a highly confidential technical product description to the CTO of NextGenSec, the selected startup for acquisition. Shortly after, you receive an email M from "OCTO@NGS.com," asking for the encrypted product details, that is signed with S and also contains the sender's public key PK as an attachment. Do you do as asked by this email?
+
+No, because you cannot establish the authenticity of the received email.
+
+
+7) What best describes the importance of digital certificates?
+Digital certificates allow the secure and practical use of public-key encryption and digital signatures by reducing the trust basis under which the underlying system is setup to operate securely.
+
+
+8) What best describes a Merkle Tree?
+A construction that employs (and generalizes the concept of) a cryptographic hash function to construct a digest d over a set of messages of size N, so that each message is linked with digest d through only about log2N other digests.
+
+
+'''
