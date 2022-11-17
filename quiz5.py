aac5018cd2fabd977ae3b8f3eae3928b0b8ec750
StevensMidnightBlessings <115166209+StevensMidnightBlessings@users.noreply.github.com>
2022-11-16 23:18:27 -0500

    Create quiz5.py

---
+++ quiz5.py
@@ -0,0 +1,29 @@
+'''
+Sorry for the late one boys
+
+
+1) To install a skill in Amazon Echo, users should download an APK file from a market store and install it in their device.
+
+False
+
+2) The inclusion of background noise actually improves data collection performance by helping keep the session alive longer.
+
+True
+
+3) Users may have different ways of communicating with an app. So a good voice app should be able to recognize the variation between users and implement them.
+
+True
+
+4) Why is the privacy of smart speaker important?
+
+Skill runs on the developer’s server as black-box; the code or document is unavailable by skills.
+
+5) To address the privacy risks of the third-party developer, Alexa relies on two ways to regulate the information and protect user privacy.
+
+(Choose two, i.e., there are two correct answers.)
+
+- Developers are not allowed to have access to raw audio. Instead, they can only access text that was transcribed from the audio by the platform provider.
+
+- Alexa limits the amount of time over which a developer can obtain a user's transcript. Based on this, a single listening session when taking user input is between 8–10 seconds
+
+'''
