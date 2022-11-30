f3e3ced80d15fe82a9cbafd27c1ea23fdc8e5831
StevensMidnightBlessings <115166209+StevensMidnightBlessings@users.noreply.github.com>
2022-11-30 15:53:56 -0500

    Create quiz6.py

---
+++ quiz6.py
@@ -0,0 +1,17 @@
+'''
+1) The Turing test was proposed by Alan Turing in 1950.
+True
+
+2) Which of the following components are parts of a machine learning problem?
+Evaluation, Estimation, Hypothesis Class
+
+3) What are the limitations of bag-of-words methods?
+Counts are not normalized, Sparsity, Ignores order of words
+
+4) What technique is discussed in the example of machine learning for education?
+Matrix Factorization ?
+
+5) What data are used in the example of machine learning for health informatics?
+Electronic health records, ICD-9-CM disease graph
+
+'''
