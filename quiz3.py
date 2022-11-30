50eef9b99a7903687789d89fabbf4ea3f7b83f18
StevensMidnightBlessings <115166209+StevensMidnightBlessings@users.noreply.github.com>
2022-11-30 15:36:48 -0500

    Create quiz3.py

---
+++ quiz3.py
@@ -0,0 +1,28 @@
+'''
+1) In programming, what is an assertion?
+    A. A Boolean expression that is supposed to be true at a certain point in the code.
+    B. A program statement used by developers to catch bugs.
+    C. A logical statement used when proving a program is correct.
+
+All of the above
+
+
+2) Which of these assertions is true following the assignment x = y+1?
+    A. x == y+1
+    B. x > y
+    
+Both of them
+
+
+3) What kind of software developers prove their programs correct?
+
+Some who develop safety-critical sofware like aircraft control.
+
+
+4) Which of these people are or were famous programmers?
+    A. Ada Lovelace.
+    B. Edsger W. Dijkstra.
+    C. Alan Turing.
+    
+ All of the above
+'''
