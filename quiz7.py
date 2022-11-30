6c5a5b32797307a76ca5e323726302ac48bcccc7
StevensMidnightBlessings <115166209+StevensMidnightBlessings@users.noreply.github.com>
2022-11-30 16:28:25 -0500

    Create quiz7.py

---
+++ quiz7.py
@@ -0,0 +1,13 @@
+'''
+1) Text-to-SQL generation is designed for question answering task on structured tables.
+True
+
+2) In Text-to-SQL generation task, the experiments show that the table name in SQL queries is the most challenging component to generate.
+False
+
+3) Clinical knowledge graph can build relationships between medical information from different patients and different clinical notes.
+True 
+
+4) In clinical knowledge base question answering task, the type and path aspects are two important factors for identifying correct answers.
+True 
+'''
