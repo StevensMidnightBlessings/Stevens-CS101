c9722433b2657189469dbcfb5b2cd4e898cb1667
StevensMidnightBlessings <115166209+StevensMidnightBlessings@users.noreply.github.com>
2022-12-07 14:56:28 -0500

    Create quiz8_attempt1.py

---
+++ quiz8_attempt1.py
@@ -0,0 +1,24 @@
+'''
+1) Modern DevOps tools mean you don't need to think about the shell.
+False
+
+2) Errors in the shell can be costly.
+True
+
+3) How does PaSh speed up shell scripts?
+Parallelizing command invocations.
+
+4) Which of the following tools will help you parse and properly process JSON files in the shell?
+grep, gron
+
+notes:
+gron - transforms grep
+ffs - filesystem better organized
+jq - more readable
+bc - faster arithmetic
+
+5) Everything we need to know about a tool can be summed up in its mechanics.
+False
+
+score 4/5
+'''
